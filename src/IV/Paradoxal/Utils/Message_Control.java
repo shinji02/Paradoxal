2ca58e3f8de8829819c55cdea5f9aeb1fe19b1e2
Serreau <jovannserreau@gmail.com>
2018-04-10 11:59:47 +0200

    Création des message dans contorl messsage

--- src/IV/Paradoxal/Utils/Message_Control.java
+++ src/IV/Paradoxal/Utils/Message_Control.java
@@ -1,33 +1,46 @@
 package IV.Paradoxal.Utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class Message_Control {
 	ConsoleCommandSender console_send = Bukkit.getConsoleSender();
 	
-	public void send_console_error() {
-		
+	public void send_console_error(String msg) {
+		console_send.sendMessage(ChatColor.GOLD+"[Zalera] "+ChatColor.RED+"[Error] "+msg+ChatColor.RESET);
 	}
 	
-	public void send_console_debug() {
-		
+	public void send_console_debug(String msg) {
+		console_send.sendMessage(ChatColor.GOLD+"[Zalera] "+ChatColor.AQUA+"[Debug] "+msg+ChatColor.RESET);
 	}
 	
-	public void send_console_succes() {
-		
+	public void send_console_succes(String msg) {
+		console_send.sendMessage(ChatColor.GOLD+"[Zalera] "+ChatColor.GREEN+"[Sucess] "+msg+ChatColor.RESET);
 	}
 	
 	public void send_player_error(Player player,String msg) {
-		
+		player.sendMessage(ChatColor.GOLD+"[Zalera] "+ChatColor.RED+msg+ChatColor.RESET);
 	}
 	
-	public void send_player_debug() {
-		
+	public void send_player_debug(Player player,String msg) {
+		player.sendMessage(ChatColor.GOLD+"[Zalera] "+ChatColor.AQUA+msg+ChatColor.RESET);
 	}
 	
-	public void send_player_success() {
-		
+	public void send_player_success(Player player, String msg) {
+		player.sendMessage(ChatColor.GOLD+"[Zalera] "+ChatColor.GREEN+msg+ChatColor.RESET);	
+	}
+	
+	public void send_player_message(Player player, String msg) {
+		player.sendMessage(ChatColor.GOLD+"[Zalera] "+msg+ChatColor.RESET);
+	}
+	
+	public void console_init(String msg) {
+		console_send.sendMessage(ChatColor.LIGHT_PURPLE+"[Init] "+ChatColor.AQUA+msg+ChatColor.RESET);
+	}
+	
+	public void console_load(String msg) {
+		console_send.sendMessage(ChatColor.LIGHT_PURPLE+"[Load] "+ChatColor.AQUA+msg+ChatColor.RESET);
 	}
 }
