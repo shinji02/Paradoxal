63d3c419a63c0f5a338e51cc56755a6ac358171d
Serreau <jovannserreau@gmail.com>
2018-04-10 09:54:49 +0200

    Création du control Message

---
+++ src/IV/Paradoxal/Utils/Message_Control.java
@@ -0,0 +1,33 @@
+package IV.Paradoxal.Utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+public class Message_Control {
+	ConsoleCommandSender console_send = Bukkit.getConsoleSender();
+	
+	public void send_console_error() {
+		
+	}
+	
+	public void send_console_debug() {
+		
+	}
+	
+	public void send_console_succes() {
+		
+	}
+	
+	public void send_player_error(Player player,String msg) {
+		
+	}
+	
+	public void send_player_debug() {
+		
+	}
+	
+	public void send_player_success() {
+		
+	}
+}
