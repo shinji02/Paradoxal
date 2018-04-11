2d6898711a954ac86a17a2f23948bfb2ec1465fe
Serreau <jovannserreau@gmail.com>
2018-04-11 12:27:59 +0200

    Préparation à la création du panel

--- src/IV/Paradoxal/Zalera/Panel/Panel.java
+++ src/IV/Paradoxal/Zalera/Panel/Panel.java
@@ -3,13 +3,31 @@ package IV.Paradoxal.Zalera.Panel;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import IV.Paradoxal.Utils.Message_Control;
 
 public class Panel implements CommandExecutor {
 
+	Message_Control msg_crtl = new Message_Control();
+	
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
-		return false;
+		if(arg0 instanceof Player) {
+			Player player = (Player)arg0;
+			if(arg1.getName().contentEquals("panel")) {
+				if(player.hasPermission("iv.panel.use"))
+				{
+					
+				}
+				else
+				{
+					
+				}
+			}
+			
+		}
+		return true;
 	}
 
 }
